35ee374999cc5d28f3fe61b8e84dca870d839bef
nragh <nragh@LAPTOP-AEOHVJQ7>
2019-08-17 14:44:32 +0530

    Actuators with default http enpoints exposed

---
+++ Actuators/src/main/java/com/mrsr/actuators/ActuatorApp.java
@@ -0,0 +1,13 @@
+package com.mrsr.actuators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApp {
+	
+	public static void main(String args[]) {
+		SpringApplication.run(ActuatorApp.class, args);
+	}
+
+}
